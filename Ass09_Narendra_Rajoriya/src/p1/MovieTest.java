73fa5c24775376d6c9f75ba48e8c8b14d8bf20d6
NARENDRA <NARENDRA@DESKTOP-QGA1LRQ>
2021-03-10 13:22:34 +0530

    Added populateMovies method

--- Ass09_Narendra_Rajoriya/src/p1/MovieTest.java
+++ Ass09_Narendra_Rajoriya/src/p1/MovieTest.java
@@ -1,5 +1,6 @@
 package p1;
 import java.util.*;
+import java.io.*;
 class Movie{
 	private int movieId;
 	private String name;
@@ -42,7 +43,7 @@ class Movie{
 	public List<String> getCasting() {
 		return casting;
 	}
-	public void setCastring(List<String> casting) {
+	public void setCasting(List<String> casting) {
 		this.casting = casting;
 	}
 	public double getRatings() {
@@ -57,8 +58,55 @@ class Movie{
 	public void setTotalBusinessDone(double totalBusinessDone) {
 		this.totalBusinessDone = totalBusinessDone;
 	}
-	
+	public String toString() {
+		return Integer.toString(this.getMovieId())+" - "+this.getName()+" - "+this.getCategory()+" - "+this.getLanguage()+" - "+this.getReleaseDate()+" - "+this.getCasting()+" - "+Double.toString(this.getRatings())+" - "+Double.toString(this.getTotalBusinessDone());
+	}
+	public static List<Movie> populateMovies(File f){
+		Movie m;
+		List<Movie>movieList = new ArrayList<Movie>();
+		try {
+			FileInputStream fi = new FileInputStream(f);
+			byte[] arr = new byte[(int)f.length()];
+			fi.read(arr);
+			String s = new String(arr);
+			System.out.println(s);
+			String[] count = s.split("\n");
+			for(int i=0;i<count.length;i++) {
+				m=new Movie();
+				String data[]=count[i].split(",");
+				m.setMovieId(Integer.parseInt(data[0]));
+				m.setName(data[1]);
+				m.setCategory(data[2]);
+				m.setLanguage(data[3]);
+				m.setReleaseDate(data[4]);
+				String[] c = data[5].split("-");
+				List<String>cast = new ArrayList<String>();
+				for(int j=0;j<c.length;j++) {
+					cast.add(c[j]);
+				}
+				m.setCasting(cast);
+				m.setRatings(Double.parseDouble(data[6]));
+				m.setTotalBusinessDone(Double.parseDouble(data[7]));
+				movieList.add(m);
+			}	
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return movieList;
+	}
+		
 }
-class MovieTest {
+	
 
+class MovieTest {
+	static List<Movie> movieList;
+	
+	public static void main(String[] args) {
+		movieList=Movie.populateMovies(new File("C:\\Users\\NARENDRA\\git\\Assignment09_Narendra_Rajoirya\\Ass09_Narendra_Rajoriya\\TextFiles\\movies.txt"));
+		Iterator<Movie> it = movieList.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
+	
 }
